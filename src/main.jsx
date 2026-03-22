77f4eb0505325bb0833342c694e717bc4473f5f7
Cihan Işık <cihan1200@outlook.com>
2026-03-22 15:47:05 +0300

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { ThemeProvider } from './contexts/ThemeContext.jsx';
-import ServerWaking from './components/server_waking/ServerWaking.jsx';
+import ServerWaking from './pages/server_waking/ServerWaking.jsx';
 import './index.css';
 import App from './App.jsx';
 
